a514032750fcf393809c6869d8e8a1ed87e37270
ASHISH KUMAR SINGH <singhashishk12@gmail.com>
2023-07-08 23:49:16 +0530

    .....

---
+++ script.js
@@ -0,0 +1,6 @@
+const accessKey = "4m_A4gaHKPAvw30bbolep5BJX9RF3ZlVk7VHuPy8Mj0"
+
+const form1 = document.querySelector("form")
+const input1 = document.querySelector("search-input")
+const searchResults = document.querySelector(".search-results")
+const showMore = document.querySelector("show-more-button")
